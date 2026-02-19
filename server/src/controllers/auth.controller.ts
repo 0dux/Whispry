98eed9550e4f78dcde229df6fb3dff3a29276378
Daksh <dakshdav@gmail.com>
2026-02-19 15:04:34 +0530

    register user controller

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,9 +1,78 @@
+import bcrypt from "bcrypt";
 import type { Request, Response } from "express";
+import zod from "zod";
+import { HttpStatusCode } from "../enums/http-status.enum.js";
+import { prisma } from "../lib/prisma.js";
+import { generateToken } from "../lib/utils.js";
+
+const registerUserSchema = zod.object({
+    name: zod.string(),
+    email: zod.email(),
+    password: zod.string().min(8),
+})
+type TRegisterUser = zod.infer<typeof registerUserSchema>;
+
+
+
 
 export const registerUser = async (req: Request, res: Response) => {
-    res.json({
-        message: "Sign-up route"
-    })
+    const data: TRegisterUser = req.body;
+    try {
+        const result = registerUserSchema.safeParse(data);
+        if (!result.success) {
+            return res.status(HttpStatusCode.BAD_REQUEST).json({
+                message: "Invalid inputs passed"
+            })
+        }
+
+        const { name, email, password } = result.data;
+
+        //check if the email is already in use
+        const user = await prisma.user.findFirst({
+            where: { email }
+        })
+        if (user) {
+            return res.status(HttpStatusCode.FORBIDDEN).json({
+                message: "User with this email already exists"
+            })
+        }
+
+        //hash the password
+        const hash = await bcrypt.hash(password, process.env.SALT_ROUNDS as string)
+
+        const newUser = await prisma.user.create({
+            data: {
+                name, email, password: hash
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                profilePicture: true,
+            }
+        })
+
+        if (!newUser) {
+            return res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({
+                message: "User creation failed."
+            })
+        }
+        generateToken(newUser.id, res)
+        return res.status(HttpStatusCode.SOMETHING_IS_CREATED).json({
+            message: "User created succesfully.",
+            user: {
+                id: newUser.id,
+                name: newUser.name,
+                email: newUser.email,
+                pfp: newUser.profilePicture,
+            }
+        })
+    } catch (error: any) {
+        console.error("Error during signing up ::", error?.message || error);
+        return res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({
+            message: "Internal server error"
+        })
+    }
 }
 
 export const logInUser = async (req: Request, res: Response) => {
---
+++ server/src/lib/utils.ts
@@ -0,0 +1,16 @@
+import { Response } from "express";
+import jwt from "jsonwebtoken";
+
+export const generateToken = async (userId: string, res: Response) => {
+    const token = jwt.sign({ userId: userId }, process.env.JWT_SECRET as string, {
+        expiresIn: "7d"
+    })
+
+    res.cookie("jwt", token, {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true,
+        sameSite: "strict",
+        secure: process.env.NODE_ENV === "PRODUCTION" ? true : false
+    })
+    return token;
+}   
